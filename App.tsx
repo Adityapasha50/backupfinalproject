4c7239832211a6ef4d3f6242356ca65e9307cd75
Adit <adityanatawijaya@gmail.com>
2025-06-29 17:33:26 +0700

    backupfp

--- App.tsx
+++ App.tsx
@@ -1,13 +1,61 @@
-import { ScreenContent } from 'components/ScreenContent';
+import React, { createContext, useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import RegisterScreen from './screens/RegisterScreen';
+import LoginScreen from './screens/LoginScreen';
+import BerandaNavigator from 'navigators/BerandaNavigator';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaView } from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
-import './global.css';
+type IAuthContext = {
+  token: string | null,
+  setToken: React.Dispatch<React.SetStateAction<string | null>>
+
+}
+const Stack = createNativeStackNavigator();
+
+export const AuthContext = createContext<IAuthContext>({
+  token: "",
+  setToken: () => { }
+})
 
 export default function App() {
+  const [token, setToken] = useState<string | null>("") // ini nanti diisi token
+
+  async function checkToken() {
+    const access_token = await SecureStore.getItemAsync("access_token")
+    const userId = await SecureStore.getItemAsync("user_id")
+    console.log(userId, "ini userId di cookies");
+
+    setToken(access_token)
+  }
+  useEffect(() => {
+    checkToken()
+  }, [])
   return (
-    <>
-      <ScreenContent title="Home" path="App.tsx"></ScreenContent>
-      <StatusBar style="auto" />
-    </>
+    <AuthContext.Provider value={{ token, setToken }}>
+
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+          {
+            token ?
+              <>
+                {/* <StatusBar style="light" /> */}
+                <Stack.Screen name='BerandaNavigator' component={BerandaNavigator} />
+              </>
+              :
+              <>
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="Register" component={RegisterScreen} />
+              </>
+          }
+
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
+
   );
 }
